2112e9e42406f3bbedc9804a5ffbe98e82adea66
Mathias Kahl <mathias.kahl@gmail.com>
2013-06-27 14:07:22 +0200

    viewport for mobile devices set

--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crowd Play ♫</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/jquery.mobile-1.3.1.min.css">
